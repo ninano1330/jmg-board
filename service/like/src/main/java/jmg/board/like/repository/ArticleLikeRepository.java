54c1ee7e9431d8dce558c3b3d78c5b12402ed4b8
ninano1330 <ninano1330@gmail.com>
2025-11-08 22:38:52 +0900

    좋아요 & 카운트 구현 - 비관적 락, 낙관적 락

---
+++ service/like/src/main/java/jmg/board/like/repository/ArticleLikeRepository.java
@@ -0,0 +1,12 @@
+package jmg.board.like.repository;
+
+import jmg.board.like.entity.ArticleLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+    Optional<ArticleLike> findByArticleIdAndUserId(Long articleId, Long userId);
+}
---
+++ service/like/src/main/java/jmg/board/like/service/ArticleLikeService.java
@@ -0,0 +1,117 @@
+package jmg.board.like.service;
+
+import jmg.board.like.entity.ArticleLike;
+import jmg.board.like.entity.ArticleLikeCount;
+import jmg.board.like.repository.ArticleLikeCountRepository;
+import jmg.board.like.repository.ArticleLikeRepository;
+import jmg.board.like.service.response.ArticleLikeResponse;
+import kuke.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleLikeService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleLikeRepository articleLikeRepository;
+    private final ArticleLikeCountRepository articleLikeCountRepository;
+
+    public ArticleLikeResponse read(Long articleId, Long userId) {
+        return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .map(ArticleLikeResponse::from)
+                .orElseThrow();
+    }
+
+    /**
+     * update 구문
+     */
+    @Transactional
+    public void likePessimisticLock1(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+
+        int result = articleLikeCountRepository.increase(articleId);
+        if(result == 0) {
+            articleLikeCountRepository.save(
+                ArticleLikeCount.init(articleId, 1L)
+            );
+        }
+    }
+
+    @Transactional
+    public void unlikePessimisticLock1(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLike -> {
+                    articleLikeRepository.delete(articleLike);
+                    articleLikeCountRepository.decrease(articleId);
+                });
+    }
+
+    /**
+     * select ... for update + update
+     */
+    @Transactional
+    public void likePessimisticLock2(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+
+        ArticleLikeCount articleLikeCount = articleLikeCountRepository.findLockedByArticleId(articleId)
+                .orElseGet(() -> ArticleLikeCount.init(articleId, 0L));
+        articleLikeCount.increase();
+        articleLikeCountRepository.save(articleLikeCount);
+    }
+
+    @Transactional
+    public void unlikePessimisticLock2(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLike -> {
+                    articleLikeRepository.delete(articleLike);
+
+                    ArticleLikeCount articleLikeCount = articleLikeCountRepository.findLockedByArticleId(articleId).orElseThrow();
+                    articleLikeCount.decrease();
+                });
+    }
+
+    @Transactional
+    public void likeOptimisticLock(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+
+        ArticleLikeCount articleLikeCount = articleLikeCountRepository.findById(articleId)
+                .orElseGet(() -> ArticleLikeCount.init(articleId, 0L));
+        articleLikeCount.increase();
+        articleLikeCountRepository.save(articleLikeCount);
+    }
+
+    @Transactional
+    public void unlikeOptimisticLock(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLike -> {
+                    articleLikeRepository.delete(articleLike);
+
+                    ArticleLikeCount articleLikeCount = articleLikeCountRepository.findById(articleId).orElseThrow();
+                    articleLikeCount.decrease();
+                });
+    }
+
+    public Long count(Long articleId) {
+        return articleLikeCountRepository.findById(articleId)
+                .map(ArticleLikeCount::getLikeCount).orElse(0L);
+    }
+}
